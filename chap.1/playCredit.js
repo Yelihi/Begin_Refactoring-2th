b39a0c15c2104c2a6feda1fab672170286a6bbd8
Yelihi <Yelihi19@gmail.com>
2022-09-28 10:45:04 +0900

    Refact: 변수 play 삭제과정

--- chap.1/playCredit.js
+++ chap.1/playCredit.js
@@ -28,9 +28,38 @@ const $plays = {
   "othe-llo": { name: "Othello", type: "tragedy" },
 };
 
-console.log($invoices.performances);
-
 const statement = (invoice, plays) => {
+  const playFor = (aPerformance) => {
+    // 임시 변수를 질의 함수로 바꾸기.
+    return plays[aPerformance.playID];
+  };
+
+  const amountFor = (play, aPerformance) => {
+    // 값이 바뀌지 않는 매개변수
+    let result = 0; // 변수를 초기화 한다. 그리고 이름을 바꾸어준다.
+    switch (playFor(aPerformance).type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(
+          `알 수 없는 장르: ${JSON.stringify(playFor(aPerformance))}`
+        );
+    }
+
+    return result; // 함수 안에서 값이 바뀌는 변수
+  };
+
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구내역 (고객명 : ${invoice.customer})\n`;
@@ -41,16 +70,16 @@ const statement = (invoice, plays) => {
   }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = amountFor(play, perf); // 추출한 함수 -> 이후 문제가 없는지 확인
+    let thisAmount = amountFor(playFor(perf), perf); // 추출한 함수 -> 이후 문제가 없는지 확인
 
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
     //희극 관객 5명마다 추가 포인트를 제공한다
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === playFor(perf).type)
+      volumeCredits += Math.floor(perf.audience / 5);
 
     //청구내역을 출력한다.
-    result += `${play.name}: ${format(thisAmount / 100)} (${
+    result += `${playFor(perf).name}: ${format(thisAmount / 100)} (${
       perf.audience
     }석)\n`;
     totalAmount += thisAmount;
@@ -59,30 +88,6 @@ const statement = (invoice, plays) => {
   result += `총액: ${format(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
-};
-
-const amountFor = (play, perf) => {
-  // 값이 바뀌지 않는 매개변수
-  let thisAmount = 0; // 변수를 초기화 한다.
-  switch (play.type) {
-    case "tragedy":
-      thisAmount = 40000;
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
-      }
-      break;
-    case "comedy":
-      thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
-      }
-      thisAmount += 300 * perf.audience;
-      break;
-    default:
-      throw new Error(`알 수 없는 장르: ${JSON.stringify(play)}`);
-  }
-
-  return thisAmount; // 함수 안에서 값이 바뀌는 변수
 };
 
 console.log(statement($invoices, $plays));
