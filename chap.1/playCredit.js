5aa242b3bf273bad80ad26d243ba5aef1cbc6d02
Yelihi <Yelihi19@gmail.com>
2022-10-02 11:59:04 +0900

    Fixed: plaintext 버전 출력부분을 다시 만듬

--- chap.1/playCredit.js
+++ chap.1/playCredit.js
@@ -30,6 +30,25 @@ const $plays = {
 };
 
 function statement(invoice, plays) {
+  return renderPlainText(createStatementData(invoice, plays));
+}
+
+function renderPlainText(data, plays) {
+  let result = `청구내역 (고객명 : ${data.customer})\n`;
+
+  for (let perf of data.performances) {
+    //청구내역을 출력한다.
+    result += `${perf.play.name}: ${usd(perf.amount / 100)}(${
+      perf.audience
+    }석)\n`;
+  }
+
+  result += `총액: ${usd(data.totalPee / 100)}\n`;
+  result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+  return result;
+}
+
+function htmlStatement(invoice, plays) {
   return renderHTML(createStatementData(invoice, plays));
 }
 
