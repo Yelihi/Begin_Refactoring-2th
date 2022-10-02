6dcf920fd200a248e8a24014b23bf74c3e0f2830
Yelihi <Yelihi19@gmail.com>
2022-10-02 11:34:02 +0900

    Refact: 데이터처리 부분 따로 함수로 분리

--- chap.1/playCredit.js
+++ chap.1/playCredit.js
@@ -29,12 +29,16 @@ const $plays = {
 };
 
 function statement(invoice, plays) {
+  return renderPlainText(createStatementData(invoice, plays));
+}
+
+function createStatementData(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
   statementData.totalVolumeCredits = totalVolumeCredits(statementData);
   statementData.totalPee = totalPee(statementData);
-  return renderPlainText(statementData, plays);
+  return statementData;
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
